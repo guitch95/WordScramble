c843ba3b1916f1b9ef8e4aea28745c987f5cddd3
guitch95 <guitch95@gmail.com>
2026-01-28 16:27:01 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Guillaume Richard on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
