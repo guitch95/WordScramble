1c961fa09aa9e58176c2318694aa37fbe833ab64
guitch95 <guitch95@gmail.com>
2026-01-29 10:42:17 +0100

    Build the function to insert in the array.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
-        .padding()
+    }
+
+    func addNewWord() {
+        // lowerCased and remove whiteSpace and NewLines.
+        let answer = newWord.lowercased().trimmingCharacters(
+            in: .whitespacesAndNewlines
+        )
+        // Make sure there is at least one character in the String answer
+        guard answer.count > 0 else { return }
+
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+
+        newWord = ""
     }
 }
 
